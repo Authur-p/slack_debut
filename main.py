7105e592719a6cff2134d950f66711c817810295
Opeyemi-daniel <opeyemi.daniel@stu.cu.edu.ng>
2023-09-09 14:26:41 +0100

    upgrade

--- main.py
+++ main.py
@@ -13,18 +13,18 @@ def slack():
     track = request.args.get('track')
     now = datetime.datetime.now()
     day = now.strftime('%A')
+    the_utc = datetime.datetime.now(pytz.utc)
+    # offset_minutes = random.randint(-2, 2)
+    # offset_time = the_utc + datetime.timedelta(minutes=offset_minutes)
 
-    offset_minutes = random.randint(-2, 2)
-
-
-    # current_utc_time = offset_time.strftime("%Y-%m-%d%H:%M:%S")
+    current_utc_time = the_utc.strftime("%Y-%m-%dT%H:%M:%SZ")
     # current_utc_time = "T".join(current_utc_time.split(' ')) + 'Z'
     # print(current_utc_time)
 
     return jsonify({
         'slack_name': slack_name,
         'current_day': day,
-        'utc_time': (datetime.datetime.now(pytz.utc) + datetime.timedelta(minutes=offset_minutes)).strftime("%Y-%m-%dT%H:%M:%SZ"),
+        'utc_time': current_utc_time,
         'track': track,
         'github_file_url': 'https://github.com/Authur-p/slack_debut/blob/main/main.py',
         'github_repo_url': 'https://github.com/Authur-p/slack_debut/tree/main',
