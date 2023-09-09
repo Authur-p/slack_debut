174318b6230674f37a44af776dd90667c5b57a84
Opeyemi-daniel <opeyemi.daniel@stu.cu.edu.ng>
2023-09-09 08:22:50 +0100

    reform

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ def slack():
     day = now.strftime('%A')
     the_utc = datetime.datetime.now(pytz.utc)
     offset_minutes = random.randint(-2, 2)
-    offset_time = now + datetime.timedelta(minutes=offset_minutes)
-    current_utc_time = offset_time.strftime('%Y-%m-%d %H:%M:%S UTC')
+    offset_time = the_utc + datetime.timedelta(minutes=offset_minutes)
+    current_utc_time = offset_time.strftime('%Y-%m-%dT%H:%M:%S UTC')
     return jsonify({
         'slack_name': slack_name,
-        'day_of_the_week': day,
-        'current_utc': current_utc_time,
+        'current_day': day,
+        'utc_time': current_utc_time,
         'track': track,
         'github_file_url': 'https://github.com/Authur-p/slack_debut/blob/main/main.py',
         'github_repo_url': 'https://github.com/Authur-p/slack_debut/tree/main',
